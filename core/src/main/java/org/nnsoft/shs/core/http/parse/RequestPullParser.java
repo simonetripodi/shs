9a54121a0e032bbcfe1eb5213f11bec163bc5aba
simonetripodi <simone.tripodi@gmail.com>
2012-05-20 16:34:04 +0200

    added missing triggers registration

--- core/src/main/java/org/nnsoft/shs/core/http/parse/RequestPullParser.java
+++ core/src/main/java/org/nnsoft/shs/core/http/parse/RequestPullParser.java
@@ -79,7 +79,7 @@ public final class RequestPullParser
         registerTrigger( new ProtocolNameParserTrigger(), PROTOCOL_NAME );
         registerTrigger( new ProtocolVersionParserTrigger(), PROTOCOL_VERSION );
         registerTrigger( new QueryStringParametersParserTrigger(), PARAM_NAME, PARAM_VALUE );
-        registerTrigger( new HeaderParserTrigger(), HEADER_NAME, HEADER_VALUE, HEADER_USER_AGENT_VALUE );
+        registerTrigger( new HeaderParserTrigger(), HEADER_NAME, HEADER_VALUE, HEADER_USER_AGENT_VALUE, HEADER_COOKIE_VALUE );
     }
 
     private void registerTrigger( ParserTrigger trigger, ParserStatus...parserStatuses )
@@ -162,7 +162,7 @@ public final class RequestPullParser
                     break;
 
                 case HEADER_VALUES_SEPARATOR:
-                    if ( HEADER_USER_AGENT_VALUE == status )
+                    if ( HEADER_USER_AGENT_VALUE == status || HEADER_COOKIE_VALUE == status )
                     {
                         append( current );
                     }
@@ -180,7 +180,9 @@ public final class RequestPullParser
 
                 case NEW_LINE:
                     tokenFound();
-                    if ( HEADER_VALUE == status || HEADER_USER_AGENT_VALUE == status )
+                    if ( HEADER_VALUE == status
+                         || HEADER_USER_AGENT_VALUE == status
+                         || HEADER_COOKIE_VALUE == status )
                     {
                         status = HEADER_NAME;
                     }
