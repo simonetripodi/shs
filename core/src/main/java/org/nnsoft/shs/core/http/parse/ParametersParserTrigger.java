ee767f3dce2bcea16e91b0cafdc44354031a5b56
simonetripodi <simone.tripodi@gmail.com>
2012-05-20 18:24:34 +0200

    initial checkin of ParametersParserTrigger class

---
+++ core/src/main/java/org/nnsoft/shs/core/http/parse/ParametersParserTrigger.java
@@ -0,0 +1,44 @@
+package org.nnsoft.shs.core.http.parse;
+
+/*
+ * Copyright (c) 2012 Simone Tripodi (simonetripodi@apache.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import static org.nnsoft.shs.core.http.parse.ParserStatus.PARAM_NAME;
+import static org.nnsoft.shs.core.http.parse.ParserStatus.PARAM_VALUE;
+
+final class ParametersParserTrigger
+    extends AbstractParametersParserTrigger
+{
+
+    public ParametersParserTrigger()
+    {
+        super( PARAM_NAME, PARAM_VALUE );
+    }
+
+    @Override
+    protected void onParameterFound( String name, String value, MutableRequest request )
+    {
+        request.addParameter( name, value );
+    }
+
+}
