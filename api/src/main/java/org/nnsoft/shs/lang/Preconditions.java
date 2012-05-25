e2a8be7d9f24969165481f7c74b919976c3b7c80
simonetripodi <simone.tripodi@gmail.com>
2012-05-25 16:23:40 +0200

    dropped unused method removed unused @throws javadoc tags

--- api/src/main/java/org/nnsoft/shs/lang/Preconditions.java
+++ api/src/main/java/org/nnsoft/shs/lang/Preconditions.java
@@ -53,10 +53,6 @@ public final class Preconditions
      * @param errorMessageArgs the arguments to be substituted into the message
      *     template. Arguments are converted to strings using
      *     {@link String#valueOf(Object)}.
-     * @throws IllegalArgumentException if {@code expression} is false
-     * @throws NullPointerException if the check fails and either {@code
-     *     errorMessageTemplate} or {@code errorMessageArgs} is null (don't let
-     *     this happen)
      */
     public static void checkArgument( boolean expression, String errorMessageTemplate, Object... errorMessageArgs )
     {
@@ -78,10 +74,6 @@ public final class Preconditions
      *     Unmatched arguments will be appended to the formatted message in square
      *     braces. Unmatched placeholders will be left as-is.
      * @param errorMessageArgs the arguments to be substituted into the message template.
-     * @throws IllegalStateException if {@code expression} is false
-     * @throws NullPointerException if the check fails and either {@code
-     *     errorMessageTemplate} or {@code errorMessageArgs} is null (don't let
-     *     this happen)
      */
     public static void checkState( boolean expression, String errorMessageTemplate, Object... errorMessageArgs )
     {
@@ -89,33 +81,6 @@ public final class Preconditions
         {
             throw new IllegalStateException( format( errorMessageTemplate, errorMessageArgs ) );
         }
-    }
-
-    /**
-     * Ensures that an object reference passed as a parameter to the calling
-     * method is not null.
-     *
-     * @param reference an object reference
-     * @param errorMessageTemplate a template for the exception message should the
-     *     check fail. The message is formed by replacing each {@code %s}
-     *     placeholder in the template with an argument. These are matched by
-     *     position - the first {@code %s} gets {@code errorMessageArgs[0]}, etc.
-     *     Unmatched arguments will be appended to the formatted message in square
-     *     braces. Unmatched placeholders will be left as-is.
-     * @param errorMessageArgs the arguments to be substituted into the message
-     *     template. Arguments are converted to strings using
-     *     {@link String#valueOf(Object)}.
-     * @return the non-null reference that was validated
-     * @throws NullPointerException if {@code reference} is null
-     */
-    public static <T> T checkNotNull( T reference, String errorMessageTemplate, Object... errorMessageArgs )
-    {
-        if ( reference == null )
-        {
-            // If either of these parameters is null, the right thing happens anyway
-            throw new NullPointerException( format( errorMessageTemplate, errorMessageArgs ) );
-        }
-        return reference;
     }
 
 }
