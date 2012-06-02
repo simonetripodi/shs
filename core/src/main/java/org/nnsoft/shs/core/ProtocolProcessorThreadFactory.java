7f0ead91b8f945fcb095addea8faa74980c0b5d3
simonetripodi <simone.tripodi@gmail.com>
2012-06-02 17:18:45 +0200

    code format

--- core/src/main/java/org/nnsoft/shs/core/ProtocolProcessorThreadFactory.java
+++ core/src/main/java/org/nnsoft/shs/core/ProtocolProcessorThreadFactory.java
@@ -37,7 +37,7 @@ final class ProtocolProcessorThreadFactory
 
     private static final String THREAD_NAME_FORMAT = "protocol-processor-%s";
 
-    final AtomicLong count = new AtomicLong(0);
+    final AtomicLong count = new AtomicLong( 0 );
 
     /**
      * {@inheritDoc}
